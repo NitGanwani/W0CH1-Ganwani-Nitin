5eb788fc26b8d04f36d7596308d1508595607d68
Nitin Ganwani <nitganwani@gmail.com>
2023-04-23 22:25:22 +0200

    Add function to play again

--- index.js
+++ index.js
@@ -56,27 +56,28 @@ const deck = [
 const randomCardButton = document.querySelector(".random-btn");
 const higherButton = document.querySelector(".higher-btn");
 const lowerButton = document.querySelector(".lower-btn");
-const quitButton = document.querySelector(".quit-btn");
 const randomCard = document.querySelector(".random-card");
+const randomCardDisplay = document.querySelector(".random-card-display");
 const newRandomCard = document.querySelector(".new-random-card");
 const guessResult = document.querySelector(".result-display");
-const hideButton = document.querySelector(".hide-btn");
+const playAgainButton = document.querySelector(".restart-btn");
 
-let randomCardDisplay = "";
+let randomCardDisplayed = "";
+let randomCardtoPlay = "";
 let newRandomCardDisplay = "";
 let resultDisplay = "";
+let points = 0;
 
 const showDisplay = () => {
-  randomCard.innerText = randomCardDisplay;
+  randomCard.innerText = randomCardDisplayed;
   newRandomCard.innerText = newRandomCardDisplay;
   guessResult.innerText = resultDisplay;
 };
 
 const handleRandomCardButton = () => {
   let generateRandomCard = deck[Math.floor(Math.random() * deck.length)];
-  randomCardDisplay = generateRandomCard;
-  randomCardDisplay = `Your card is the ${randomCardDisplay.card} of ${randomCardDisplay.type}.`;
-  newRandomCardDisplay = "";
+  randomCardtoPlay = generateRandomCard;
+  randomCardDisplayed = `Your card is the ${randomCardtoPlay.card} of ${randomCardtoPlay.type}.`;
   return showDisplay();
 };
 
@@ -87,11 +88,14 @@ const getNewRandomCard = () => {
 
 const handleHigherButton = () => {
   getNewRandomCard();
-  if (newRandomCardDisplay.value > randomCardDisplay.value) {
+  if (newRandomCardDisplay.value > randomCardtoPlay.value) {
     newRandomCardDisplay = `Congrats!! Your guess was right. The card is the ${newRandomCardDisplay.card} of ${newRandomCardDisplay.type}.`;
+  } else if (newRandomCardDisplay.value === randomCardtoPlay.value) {
+    newRandomCardDisplay = `Wow, this is not usual. We have a tie! You neither win nor lose. The card is the ${newRandomCardDisplay.card} of ${newRandomCardDisplay.type}.`;
   } else {
     newRandomCardDisplay = `Oooooh. 😢😢 Your guess was wrong. The card is the ${newRandomCardDisplay.card} of ${newRandomCardDisplay.type}.`;
   }
+  randomCardButton.disabled = true;
   higherButton.disabled = true;
   lowerButton.disabled = true;
   return showDisplay();
@@ -99,17 +103,20 @@ const handleHigherButton = () => {
 
 const handleLowerButton = () => {
   getNewRandomCard();
-  if (newRandomCardDisplay.value < randomCardDisplay.value) {
+  if (newRandomCardDisplay.value < randomCardtoPlay.value) {
     newRandomCardDisplay = `Congrats!! Your guess was right. The card is the ${newRandomCardDisplay.card} of ${newRandomCardDisplay.type}.`;
+  } else if (newRandomCardDisplay.value === randomCardtoPlay.value) {
+    newRandomCardDisplay = `Wow, this is not usual. We have a tie! You neither win nor lose. The card is the ${newRandomCardDisplay.card} of ${newRandomCardDisplay.type}.`;
   } else {
     newRandomCardDisplay = `Oooooh. 😢😢 Your guess was wrong. The card is the ${newRandomCardDisplay.card} of ${newRandomCardDisplay.type}.`;
   }
+  randomCardButton.disabled = true;
   higherButton.disabled = true;
   lowerButton.disabled = true;
   return showDisplay();
 };
 
-const registerEventListeners = () => {
+const startCardGame = () => {
   randomCardButton.addEventListener("click", () => {
     handleRandomCardButton();
   });
@@ -121,6 +128,21 @@ const registerEventListeners = () => {
   lowerButton.addEventListener("click", () => {
     handleLowerButton();
   });
+
+  playAgainButton.addEventListener("click", () => {
+    handlePlayAgainButton();
+  });
 };
 
-registerEventListeners();
+const handlePlayAgainButton = () => {
+  randomCardDisplayed = "";
+  randomCardtoPlay = "";
+  newRandomCardDisplay = "";
+  showDisplay();
+  randomCardButton.disabled = false;
+  higherButton.disabled = false;
+  lowerButton.disabled = false;
+  startCardGame();
+};
+
+startCardGame();
